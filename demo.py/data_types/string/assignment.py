2949382f16e0b36aec95771871995234a0832615
demo <dazanderofficials@gmail.com>
2024-04-01 16:07:27 +0800

    Create a variable of type ' str '

---
+++ demo.py/data_types/string/assignment.py
@@ -0,0 +1,19 @@
+string : str = "a"
+print(string)
+
+# Output :
+# a
+
+
+string : str = "this is a string"
+print(string)
+
+# Output :
+# this is a string
+
+
+string : str = "this variable is of type string"
+print(type(string))
+
+# Output :
+# <class 'str'>
