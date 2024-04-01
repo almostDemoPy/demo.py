8b7718dcfc273f7d8085b49384b2eceb284f7a58
demo <dazanderofficials@gmail.com>
2024-04-01 16:27:48 +0800

    Capitalize the first character of a string

---
+++ demo.py/data_types/string/capitalize().py
@@ -0,0 +1,20 @@
+"""
+
+str.capitalize()
+
+- returns a copy of the string with the first character of the string capitalized
+
+"""
+
+string : str = "hello world"
+print(string.capitalize())
+
+# Output :
+# Hello world
+
+
+string : str = "demo.py"
+print(string.capitalize())
+
+# Output :
+# Demo.py
