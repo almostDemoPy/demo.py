c6bc4117ce34bcfb7709298f8e8af910a9a9b9c0
demo <dazanderofficials@gmail.com>
2024-05-09 18:28:13 +0800

    Convert all characters of a string to lowercase

---
+++ demo.py/data_types/string/methods/lower().py
@@ -0,0 +1,16 @@
+"""
+
+str.lower()
+
+- Convert all characters of a string to lowercase
+
+"""
+
+string : str = "Sample String"
+print(string.lower())
+# "sample string"
+
+
+string : str = "SAMPLE STRING"
+print(string.lower())
+# "sample string"
