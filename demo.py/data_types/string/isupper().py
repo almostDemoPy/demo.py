72f61080ca72625b1925531072ceececfe2537d3
demo <dazanderofficials@gmail.com>
2024-05-09 17:44:42 +0800

    Check whether all characters in a string are uppercase or not

---
+++ demo.py/data_types/string/isupper().py
@@ -0,0 +1,23 @@
+"""
+
+str.isupper()
+
+- Check whether all characters in a string are uppercase or not
+
+"""
+
+string : str = "SAMPLE STRING"
+print(string.isupper())
+# True
+
+
+string : str = "Sample String"
+print(string.isupper())
+# False
+
+
+# Case : digits
+
+string : str = "123"
+print(string.isupper())
+# False
