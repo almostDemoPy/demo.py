bec7523b73f304c7f275df9ae49890025b47b884
demo <dazanderofficials@gmail.com>
2024-05-24 17:32:23 +0800

    Convert all lowercase characters into uppercase characters

---
+++ demo.py/data_types/string/methods/upper.py
@@ -0,0 +1,16 @@
+"""
+
+str.upper()
+
+- Converts all lowercase characters into uppercase characters
+
+"""
+
+string : str = "Sample string"
+print(string.upper())
+# "SAMPLE STRING"
+
+
+string : str = "SAMPLE string"
+print(string.upper())
+# "SAMPLE STRING"
