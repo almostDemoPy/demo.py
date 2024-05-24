000579a218a70c883dc17eedf456a140c5dc1ec6
demo <dazanderofficials@gmail.com>
2024-05-24 17:30:16 +0800

    Convert the first letter of every word to uppercase and keeping the rest to lowercase

---
+++ demo.py/data_types/string/methods/title.py
@@ -0,0 +1,22 @@
+"""
+
+str.title()
+
+- Convert the first letter of every word to uppercase and
+    keeping the rest to lowercase
+
+"""
+
+string : str = "sampLE StRIng"
+print(string.title())
+# "Sample String"
+
+
+string : str = "sample string"
+print(string.title())
+# "Sample String"
+
+
+string : str = "SAMPLE STRING"
+print(string.title())
+# "Sample String"
