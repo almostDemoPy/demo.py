8caae79281b09677a014d800f022af2eea7858d3
demo <dazanderofficials@gmail.com>
2024-05-24 13:53:23 +0800

    Split the string at the first occurence of the separator

--- demo.py/data_types/string/methods/partition().py
+++ demo.py/data_types/string/methods/partition().py
@@ -12,9 +12,31 @@ Parameters :
 
 separator : str = substring that separates the string
 
+===============================================================
+
+Exceptions :
+
+ValueError : empty separator
+
 """
 
 string : str = "sample python string"
 separator : str = "python"
-print(string.separator)
+print(string.partition(separator))
 # ("sample ", "python", " string")
+
+
+# Case : separator is not found in the string
+
+string : str = "sample string"
+separator : str = "of"
+print(string.partition(separator))
+# ("sample string", "", "")
+
+
+# Case : separator is an empty string
+
+string : str = "sample string"
+separator : str = ""
+print(string.partition(separator))
+# ValueError: empty separator
