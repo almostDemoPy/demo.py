009da033255c4018edc22459b106c88b426e01d5
demo <dazanderofficials@gmail.com>
2024-05-24 13:19:52 +0800

    Split the string at the first occurence of the separator

---
+++ demo.py/data_types/string/methods/partition().py
@@ -0,0 +1,20 @@
+"""
+
+str.partition(
+  separator : str
+)
+
+- Split the string at the first occurence of the separator
+
+===============================================================
+
+Parameters :
+
+separator : str = substring that separates the string
+
+"""
+
+string : str = "sample python string"
+separator : str = "python"
+print(string.separator)
+# ("sample ", "python", " string")
