fb4bc8714b1c88d7b8526deec79082e103d4d85a
demo <dazanderofficials@gmail.com>
2024-06-05 17:22:12 +0800

    Return a shallow copy of the list

---
+++ demo.py/data_types/list/methods/copy.py
@@ -0,0 +1,14 @@
+"""
+
+list.copy()
+
+- Return a shallow copy of the list
+
+"""
+
+sample_list : list = [1, 2, 3]
+sample_list_copy : list = sample_list.copy()
+print(sample_list)
+# [1 ,2 ,3]
+print(sample_list_copy)
+# [1, 2, 3]
