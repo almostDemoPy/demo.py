97056ba99ab3e9302f5fa63a30c47707f37fcf36
demo <dazanderofficials@gmail.com>
2024-06-06 20:31:52 +0800

    list indexing

---
+++ demo.py/data_types/list/indexing.py
@@ -0,0 +1,18 @@
+"""
+
+List indexing in Python :
+
+list[int]
+
+"""
+
+sample_list : list[str] = ["a", "b", "c"]
+print(sample_list[1])
+# "b"
+
+
+# Case : negative integer
+
+sample_list : list[str] = ["a", "b", "c"]
+print(sample_list[-1])
+# "c"
