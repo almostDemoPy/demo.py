24d087df345b7c2f790daa2c1b9df9275cfac14c
demo <dazanderofficials@gmail.com>
2024-06-07 12:58:30 +0800

    Returns a list with all dictionary keys with values

---
+++ demo.py/data_types/dictionary/methods/items.py
@@ -0,0 +1,15 @@
+"""
+
+dictionary.items()
+
+- Returns a list with all dictionary keys with values
+
+"""
+
+dictionary : dict[int, int] = {
+  1 : 2,
+  3 : 4,
+  5 : 6
+}
+print(dictionary.items())
+# [(1, 2), (3, 4), (5, 6)]
