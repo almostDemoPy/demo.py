eeec174968d3a209a3f36aebf395eec2df8bbc25
demo <dazanderofficials@gmail.com>
2024-07-03 20:11:32 +0800

    /level info ; slash command added

---
+++ demo.py/packages/discord.py/commands/economy/level.py
@@ -0,0 +1,62 @@
+import discord
+import json
+import traceback
+from discord import app_commands, ui
+from discord.ext import commands
+from typing import TextIO
+
+class LevelCog(commands.Cog):
+  def __init__(self, bot : commands.Bot) -> None:
+    self.bot : commands.Bot = bot
+    self.experience_gain_amount : int = 5
+    self.multiplier : int = 4
+
+  level : app_commands.Group = app_commands.Group(
+    name = "level",
+    description = "/level"
+  )
+
+  def get_max_exp(self, file : TextIO, user_id : int) -> int:
+    level : int = file.get(str(user_id), {"level": 1, "experience": 0})["level"]
+    max_experience : int = int((level + 1) ** self.multiplier)
+    return max_experience
+
+  def load_file(self, path : str) -> TextIO:
+    with open(path, "r") as f:
+      file : TextIO = json.load(f)
+    return file
+
+  @level.command(
+    name = "info",
+    description = "Check a user's current level"
+  )
+  @app_commands.describe(
+    member = "Select another member :"
+  )
+  async def level_info(self, interaction : discord.Interaction, member : discord.Member | None = None) -> None:
+    file : TextIO = self.load_file("json/level.json")
+    user : discord.Member = member or interaction.user
+    user_data : dict[str, int] = file.get(str(user.id), {"level": 1, "experience": 0})
+    max_experience : int = self.get_max_exp(file, user.id)
+    await interaction.response.send_message(
+      embed = discord.Embed(
+        description = f"**Level** : ` {user_data["level"]:,} `\n" \
+          f"**Experience** : ` {user_data["experience"]:,} | {max_experience:,} `",
+        color = 0x2b2d31
+      ).set_author(
+        name = user.name,
+        icon_url = user.display_avatar
+      ).set_thumbnail(
+        url = user.display_avatar
+      ).set_footer(
+        text = f"User ID : {user.id}"
+      ),
+      ephemeral = True
+    )
+
+  @level_info.error
+  async def error(self, interaction : discord.Interaction, error : Exception) -> None:
+    traceback.print_exc()
+
+async def setup(bot : commands.Bot) -> None:
+  await bot.add_cog(LevelCog(bot))
