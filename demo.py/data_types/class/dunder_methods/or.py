d9b7adb2f5dffcd70debad23463899fdf1c53e39
demo <dazanderofficials@gmail.com>
2024-07-11 11:44:36 +0800

    Implements the built-in Bitwise OR | operation

---
+++ demo.py/data_types/class/dunder_methods/or.py
@@ -0,0 +1,23 @@
+"""
+
+class ClassName:
+  def __or__(self, other : Any) -> Any:
+    ...
+
+- Implements the built-in Bitwise OR | operation
+
+"""
+
+from typing import Self
+
+class SampleClass:
+  def __init__(self, number : int) -> None:
+    self.number : int = number
+
+  def __or__(self, other : Self) -> int:
+    return self.number | other.number
+
+object_one : SampleClass[int] = SampleClass(32)
+object_two : SampleClass[int] = SampleClass(16)
+print(object_one | object_two)
+# 48
