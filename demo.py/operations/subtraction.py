1e734da56766cef43d45470559beac145142d550
demo <dazanderofficials@gmail.com>
2024-07-25 17:35:03 +0800

    Subtract two operands

---
+++ demo.py/operations/subtraction.py
@@ -0,0 +1,23 @@
+"""
+
+x - y
+
+- Subtract two operands
+
+"""
+
+# float
+
+a : float = 1.0
+b : float = 0.5
+print(a - b)
+# 0.5
+
+# =============================================================
+
+# integer
+
+a : int = 5
+b : int = 3
+print(a - b)
+# 2
