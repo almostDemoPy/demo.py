ea18d0586cdce2855b62e94c1f5666b787be8e47
demo <dazanderofficials@gmail.com>
2024-07-28 12:57:48 +0800

    Returns True if the left operand is greater than the right

---
+++ demo.py/operations/comparison/greater_than.py
@@ -0,0 +1,24 @@
+"""
+
+x > y
+
+- Returns True if the left operand is greater than the right
+
+"""
+
+print(
+  3 > 5
+)
+# False
+
+
+print(
+  3.0 > 3
+)
+# False
+
+
+print(
+  3.0 > 2.9
+)
+# True
