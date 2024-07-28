eb0c68481358c887417fcf24aab428eac7875d1d
demo <dazanderofficials@gmail.com>
2024-07-28 13:07:08 +0800

    Returns True if the left operand is less than or equal to the right

---
+++ demo.py/operations/comparison/less_than_or_equal_to.py
@@ -0,0 +1,25 @@
+"""
+
+x <= y
+
+- Returns True if the left operand is less than or equal to
+    the right
+
+"""
+
+print(
+  3 <= 5
+)
+# True
+
+
+print(
+  3.0 <= 3
+)
+# True
+
+
+print(
+  3.0 <= 2.9
+)
+# False
