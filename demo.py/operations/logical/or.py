edfa3390b1937edf9484615056cb9b55b10423ec
demo <dazanderofficials@gmail.com>
2024-07-29 11:23:20 +0800

    Returns True if either of the operands is true

---
+++ demo.py/operations/logical/or.py
@@ -0,0 +1,24 @@
+"""
+
+x or y
+
+- Returns True if either of the operands is true
+
+"""
+
+print(
+  True or True
+)
+# True
+
+
+print(
+  True or False
+)
+# True
+
+
+print(
+  False or False
+)
+# False
