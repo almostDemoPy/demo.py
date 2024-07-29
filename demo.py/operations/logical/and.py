be3c367e3a6a56917bf64869dc0ef93e73785cac
demo <dazanderofficials@gmail.com>
2024-07-29 11:21:15 +0800

    Returns True if both operands are true

---
+++ demo.py/operations/logical/and.py
@@ -0,0 +1,24 @@
+"""
+
+x and y
+
+- Returns True if both the operands are true
+
+"""
+
+print(
+  True and True
+)
+# True
+
+
+print(
+  True and False
+)
+# False
+
+
+print(
+  False and False
+)
+# False
