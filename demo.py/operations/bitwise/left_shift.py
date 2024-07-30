d23d66f1f0f18a47681d8621073eaf61884a0eb7
demo <dazanderofficials@gmail.com>
2024-07-30 11:55:45 +0800

    Returns the x with the bits shifted to the left by y places ( and new bit son the right-hand-side are zeros ). This is the same as multiplying x by 2 ** y

---
+++ demo.py/operations/bitwise/left_shift.py
@@ -0,0 +1,14 @@
+"""
+
+x << y
+
+- Returns the x with the bits shifted to the left by y places
+    ( and new bits on the right-hand-side are zeros ). This is
+    the same as multiplying x by 2 ** y
+
+"""
+
+print(
+  10 << 4
+)
+# 160
