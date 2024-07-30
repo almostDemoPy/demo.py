7de2e11961b45b7df2df65a9e7c82daa6cb2fbaf
demo <dazanderofficials@gmail.com>
2024-07-30 11:48:58 +0800

    Each bit of the output is the same as the corresponding bit in x if that bit in y is 0, and it's the complement of the bit in x if that bit in y is 1

---
+++ demo.py/operations/bitwise/xor.py
@@ -0,0 +1,14 @@
+"""
+
+x ^ y
+
+- Each bit of the output is the same as the corresponding bit
+    in x if that bit in y is 0, and it's the complement of the
+    bit in x if that bit in y is 1
+
+"""
+
+print(
+  10 ^ 4
+)
+# 14
