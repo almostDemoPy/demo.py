62484da83534efe124f0fa21ed0e7ee1212efdd4
demo <dazanderofficials@gmail.com>
2024-07-30 11:45:11 +0800

    Returns the copmlement of x - the number you get by switching each 1 for a 0 and each 0 for a 1. This is the same as -x - 1

---
+++ demo.py/operations/bitwise/not.py
@@ -0,0 +1,20 @@
+"""
+
+~x
+
+- Returns the complement of x - the number you get by switching
+    each 1 for a 0 and each 0 for a 1. This is the same as
+    -x - 1
+
+"""
+
+print(
+  ~10
+)
+# -11
+
+
+print(
+  ~4
+)
+# -5
