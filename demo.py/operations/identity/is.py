b7a0ca40b08c29ea677db708bacdd52b35d7d48d
demo <dazanderofficials@gmail.com>
2024-08-03 17:42:00 +0800

    Returns True if both operands are identical in memory

---
+++ demo.py/operations/identity/is.py
@@ -0,0 +1,23 @@
+"""
+
+x is y
+
+- Returns True if both operands are identical in memory
+
+"""
+
+a = 10
+b = 10
+print(a is b)
+# True
+
+
+a = 10
+b = 10
+print(a is b)
+# True
+
+a = 10
+b = 20
+print(a is b)
+# False
