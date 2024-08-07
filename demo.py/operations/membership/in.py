6bab41ac728be7a7790fb761550586d19135be55
demo <dazanderofficials@gmail.com>
2024-08-07 12:45:38 +0800

    Returns True if value is found in the sequence

---
+++ demo.py/operations/membership/in.py
@@ -0,0 +1,16 @@
+"""
+
+x in sequence
+
+- Returns True if value is found in the sequence
+
+"""
+
+my_list : list[int] = [ 1, 2, 3 ]
+print(2 in my_list)
+# True
+
+
+my_list : list[int] = [ "python", "cpp", "java" ]
+print("javascript" in my_list)
+# False
