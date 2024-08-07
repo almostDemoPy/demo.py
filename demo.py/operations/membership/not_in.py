140bb8b95c3d330a9b95c713b51fbc77cab00508
demo <dazanderofficials@gmail.com>
2024-08-07 12:46:37 +0800

    Returns True if value is not found in the sequence

---
+++ demo.py/operations/membership/not_in.py
@@ -0,0 +1,16 @@
+"""
+
+x not in sequence
+
+- Returns True if value is not found in the sequence
+
+"""
+
+my_list : list[int] = [ 1, 2, 3 ]
+print(2 not in my_list)
+# False
+
+
+my_list : list[int] = [ "python", "cpp", "java" ]
+print("javascript" not in my_list)
+# True
