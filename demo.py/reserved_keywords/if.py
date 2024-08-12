bb92940869c17a7ec530bc0eecae473e04c2c9f2
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-12 13:04:11 +0800

    Used to construct an if-statement

---
+++ demo.py/reserved_keywords/if.py
@@ -0,0 +1,14 @@
+"""
+
+if
+
+- Used to construct an if-statement
+
+"""
+
+number : int | float = -1
+is_negative : bool = False
+if number < 0:
+  is_negative : bool = True
+print(is_negative)
+# True
