62e3f5fbe592ab3cb7f15e664c9712e547adb075
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-13 12:09:58 +0800

    Used to exit the function or return a value

---
+++ demo.py/reserved_keywords/return.py
@@ -0,0 +1,17 @@
+"""
+
+return
+
+- Used to exit the function or return a value
+
+"""
+
+
+def multiply(
+  x : int | float,
+  y : int | float
+) -> int | float:
+  return x * y
+
+print(multiply(2, 5))
+# 10
