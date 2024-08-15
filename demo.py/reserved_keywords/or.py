08348b686e86e4c954ac02dbb93682e78e9ff4c4
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-15 10:51:17 +0800

    Returns True if either of the values are True

---
+++ demo.py/reserved_keywords/or.py
@@ -0,0 +1,23 @@
+"""
+
+or
+
+- Returns True if either of the values are
+    True
+
+"""
+
+print(True or True)
+# True
+
+
+print(True or False)
+# True
+
+
+print(False or True)
+# True
+
+
+print(False or False)
+# False
