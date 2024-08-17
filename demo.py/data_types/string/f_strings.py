7728143447734997c166405bc50dc154f9036dab
Demoutrei Korasaki <dazanderofficials@gmail.com>
2024-08-17 16:37:30 +0800

    A quicker way to declare a formatted string

---
+++ demo.py/data_types/string/f_strings.py
@@ -0,0 +1,13 @@
+"""
+
+string : str = f"..."
+
+- A quicker way to declare a formatted string
+
+"""
+
+name : str = "Alice"
+age : int = 19
+
+print(f"My name is {name} and I'm {age:,} years old !")
+# "My name is Alice and I'm 19 years old !"
